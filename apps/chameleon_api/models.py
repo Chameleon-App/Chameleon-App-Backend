28f22810276714e268ff9aa98ef66399960e46f1
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2023-11-05 17:09:28 +0300

    :sparkles: [add] Proto and DailyColors models

--- apps/chameleon_api/models.py
+++ apps/chameleon_api/models.py
@@ -1,5 +1,21 @@
+import datetime
 from django.db import models
+from django.utils.translation import gettext as _
+
+
+class PantoneColorPrototype(models.Model):
+    hex = models.CharField(max_length=255)
 
 
 class PantoneColor(models.Model):
     name = models.CharField(max_length=255)
+    number = models.CharField(max_length=255)
+    hex = models.CharField(max_length=255)
+    prototype = models.ForeignKey(
+        PantoneColorPrototype, on_delete=models.SET_NULL, null=True
+    )
+
+
+class DailyColors(models.Model):
+    date = models.DateField(_("Date"), default=datetime.date.today)
+    colors = models.ManyToManyField(PantoneColor)
