95ed9d88b532b44c955e2cb86aebdd2816a357b3
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2023-11-05 17:11:18 +0300

    :hammer: [add] models to register

--- apps/chameleon_api/admin.py
+++ apps/chameleon_api/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import PantoneColor
+from .models import PantoneColor, PantoneColorPrototype, DailyColors
 
-admin.site.register([PantoneColor])
+admin.site.register([PantoneColor, PantoneColorPrototype, DailyColors])
