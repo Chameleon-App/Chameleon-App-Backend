dd8ad1f5298310e44347360ebda0088ba6d180bf
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2023-11-12 18:50:38 +0300

    [add] new endpoints (aka ruchki) for photos

--- apps/chameleon_api/urls.py
+++ apps/chameleon_api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from django.contrib.auth.decorators import login_required
+from django.conf.urls.static import static
+from django.conf import settings
 from .views import *
 
 urlpatterns = [
     path("colors/list/", ColorListView.as_view()),
+    path("colors/today/", DailyColorsView.as_view()),
+    path("photos/rate/", PhotoRatingView.as_view()),
+    path("photos/today/", TodayPhotoRating.as_view()),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
