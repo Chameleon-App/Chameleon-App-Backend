8f5d4bc696631cf4f27c65bfc872680c027a9fce
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2023-11-13 11:46:58 +0300

    [fix] remove external admin access

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -30,13 +30,16 @@ schema_view = get_schema_view(
     public=True,
 )
 
-
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/", include("apps.chameleon_api.urls")),
     path(
         "doc/swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
-    path("", include("apps.chameleon_api.urls")),
 ]
+
+if os.environ["MODE"] == "0":
+    urlpatterns += [
+        path("admin/", admin.site.urls),
+    ]
