49f3151ea48f77f20ae29e2b440b2afb60f5029a
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2023-12-14 23:50:10 +0300

    :hammer: [fix] doc to docs

--- config/urls.py
+++ config/urls.py
@@ -35,7 +35,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("api/", include("apps.chameleon_api.urls")),
     path(
-        "doc/swagger/",
+        "docs/swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
