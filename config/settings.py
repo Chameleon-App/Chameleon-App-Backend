98d823086e25fd0e80f3250ff63c7882f30e7b38
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2023-12-19 19:39:32 +0300

    :hammer: [fix] str 2 bool cast in config

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import ast
 import os
 from pathlib import Path
 
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DJANGO_DEBUG")
+DEBUG = ast.literal_eval(os.getenv("DJANGO_DEBUG"))
 
 ALLOWED_HOSTS = ["*"]
 
